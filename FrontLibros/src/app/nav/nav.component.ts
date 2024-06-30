7580aa983e26ad12c11ffb0dae1acb8ec71bc371
victorarvalo@yahoo.com <victorarvalo@yahoo.com>
2024-06-30 11:37:58 -0500

    Se realiza la creación del enpoint category con salida y entradas con DTOCategory

--- FrontLibros/src/app/nav/nav.component.ts
+++ FrontLibros/src/app/nav/nav.component.ts
@@ -39,8 +39,8 @@ export class NavComponent implements OnInit {
         data=>{
           if(data.status == 200){
             this._sessionStorageService.clear();
-            window.location.reload();
             this.router.navigate(['login']);
+            window.location.reload();
           }
       },
     error=>{
